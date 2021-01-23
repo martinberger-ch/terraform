1aea6b9bc50ab211a009be9802b6efcd8e1c2c6f
martinberger-ch <56437524+martinberger-ch@users.noreply.github.com>
2021-01-23 14:37:21 +0100

    .gitignore is now working

--- 005-infrastructure/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "http" {
-    update_method = "PUT"
-    address       = "https://objectstorage.eu-zurich-1.oraclecloud.com/p/bEfUlAV0bDEfyoWstXlcYtDemHHV5pl1nEgQUUH7AA6uCEcGcqzIFefbHrOSTm9o/n/zrq07gukammy/b/terraform_state_file/o/terraform.tfstate"
-  }
-}
