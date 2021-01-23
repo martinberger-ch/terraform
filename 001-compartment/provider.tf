1d8c59d270fd87799261db9512f2d4133ce871e3
martinberger-ch <56437524+martinberger-ch@users.noreply.github.com>
2021-01-23 14:41:16 +0100

    added to gitignore

--- 001-compartment/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaxuk4je4t3aorovuzmwyeaq5sftqv3nkyz64snijtdkitadbqqxmq"
-  user_ocid = "ocid1.user.oc1..aaaaaaaadlv5vwm3zfdnyanosmdjm7cyjzdhmgawregvm6de7wznb3fjqtxq"
-  private_key_path = "/home/oci/.oci/oci_api_key.pem"
-  fingerprint = "6a:ce:8e:a7:4a:a1:70:ad:4e:d7:e6:8f:d6:97:55:60"
-  region = "eu-zurich-1"
-}
