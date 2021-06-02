f91f85624c4193ec34e54ee7135f1a4e89383755
martinberger-ch <56437524+martinberger-ch@users.noreply.github.com>
2021-06-02 12:41:46 +0200

    changed vars

--- oci-tf-demo/compute.tf
+++ oci-tf-demo/compute.tf
@@ -35,3 +35,14 @@ resource "oci_core_public_ip" "demo_public_ip" {
   private_ip_id  = ""
 }
  
+data "oci_core_private_ips" "demo_public_ips" {
+ip_address = oci_core_instance.demo_public_ip.private_ip
+subnet_id  = oci_core_subnet.vcn-public-subnet.id
+}
+
+resource "oci_core_public_ip" "demo_public_ip-1" {
+compartment_id = var.compartment_id
+display_name   = "reserved public ip"
+lifetime       = "RESERVED"
+private_ip_id  = data.oci_core_private_ips.demo_public_ips.private_ips[0]["id"]
+}
