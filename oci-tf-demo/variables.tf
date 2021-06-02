768b9f5e7ffd4f503d2adf958ab9fd18cf90c65b
martinberger-ch <56437524+martinberger-ch@users.noreply.github.com>
2021-06-02 11:10:41 +0200

    added vars

--- oci-tf-demo/variables.tf
+++ oci-tf-demo/variables.tf
@@ -42,12 +42,52 @@ variable "vcn_cidr" {
 }
 
 
+variable "vcn_public_cidr_block" {
+    description = "cidr block of public subnet"
+    default     = "10.1.0.0/27"
+    type        = string
+}
+
+variable "vcn_private_cidr_block" {
+    description = "cidr block of private subnet"
+    default     = "10.1.0.32/27"
+    type        = string
+}
+
+variable "nat_gateway_display_name" {
+    description = "Display name of NAT gateway"
+    default     = "ng-01"
+    type        = string
+}
+
+variable "internet_gateway_display_name" {
+    description = "Display name of internet gateway"
+    default     = "ig-01"
+    type        = string
+}
+
+variable "service_gateway_display_name" {
+    description = "Display name of internet gateway"
+    default     = "sg-01"
+    type        = string
+}
+
 variable "service_gateway_enabled" {
     description = "Service gateway enabled"
     default     = true
     type        = bool
 }
 
+variable "public_route_table_display_name" {
+    description = "Display name of the public route table"
+    default     = "rt-public-01"
+    type        = string
+}
+
+variable "private_route_table_display_name" {
+    description = "Display name of the private route table"
+    default     = "rt-private-01"
+    type        = 
 
 # general oci parameters ----------------------------------------------------
 variable "compartment_id" {
