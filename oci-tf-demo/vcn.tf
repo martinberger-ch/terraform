27f911aea0cdac376308fdcebe3f715ae4e4f9de
martinberger-ch <56437524+martinberger-ch@users.noreply.github.com>
2021-06-02 10:07:42 +0200

    added new files

--- oci-tf-demo/vcn.tf
+++ oci-tf-demo/vcn.tf
@@ -1,3 +1,4 @@
+# vcn -----------------------------------------------------------------------
 module "vcn"{
   source  = "oracle-terraform-modules/vcn/oci"
   version = "1.0.3"
@@ -9,10 +10,35 @@ module "vcn"{
   region = var.region
   vcn_name = var.vcn_name
   vcn_dns_label = var.vcn_dns_label
+}
 
+# public subnet----------------------------------------------------------------
+resource "oci_core_subnet" "vcn-public-subnet"{
+
+  # Required
+  compartment_id = oci_identity_compartment.tf-compartment.id
+  vcn_id = module.vcn.vcn_id
+  cidr_block = var.vcn_public_cidr_block
+ 
   # Optional
-  internet_gateway_enabled = true
-  nat_gateway_enabled = true
-  service_gateway_enabled = true
+  route_table_id = oci_core_route_table.public_route_table.id
+  security_list_ids = [oci_core_security_list.public-security-list.id]
+  display_name = "sn-regional-public-01"
+}
 
+
+# private subnet--------------------------------------------------------------
+resource "oci_core_subnet" "vcn-private-subnet"{
+
+  # Required
+  compartment_id = oci_identity_compartment.tf-compartment.id
+  vcn_id = module.vcn.vcn_id
+  cidr_block = var.vcn_private_cidr_block
+ 
+  # Optional
+  # Caution: For the route table id, use module.vcn.nat_route_id.
+  # Do not use module.vcn.nat_gateway_id, because it is the OCID for the gateway and not the route table.
+  route_table_id = oci_core_route_table.private_route_table.id
+  security_list_ids = [oci_core_security_list.private-security-list.id]
+  display_name = "sn-regional-private-01"
 }
